f89548439ac02cdac5a8822c423cf9019f340630
yatne <antonimaciej@gmail.com>
2023-02-17 15:32:59 +0100

    iframe for itch.io + focus on enter game

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {SuperSlideMe} from 'react-super-slide-me';
 import {Link} from 'react-router-dom';
 
@@ -9,11 +9,18 @@ interface Props {
 
 const GamePage = (props: Props) => {
   let customLevels = props.levels ? props.levels : undefined;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const ele = document.getElementById(props.id)?.children[0];
+    // @ts-ignore
+    ele.focus()
+  }, [ref])
 
   return (
     <>
       <Link to="/"><div className="backArrow">← Back</div></Link>
-      <div className="gameContainer">
+      <div className="gameContainer" tabIndex={0} ref={ref}>
         <SuperSlideMe
           id={props.id}
           customLevels={customLevels}
